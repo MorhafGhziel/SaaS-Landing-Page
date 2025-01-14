d71d5c9a0ef94b5e77dd7c4281ba4026e2220989
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-14 22:29:52 +0300

    added the layers logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Image from "next/image";
+import logoImage from "@/public/logo.svg";
 
 const Navbar = () => {
   return (
     <section>
       <div className="container">
         <div className="grid grid-cols-2">
-          <div></div>
+          <div>
+            <Image src={logoImage} alt="Layers Logo" />
+          </div>
           <div></div>
         </div>
       </div>
