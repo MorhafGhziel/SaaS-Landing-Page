b312252ff1b9ee7cef3122a70d0305a51d1d3411
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-14 23:39:10 +0300

    done the custom button component

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { HTMLAttributes } from "react";
+import { cva } from "class-variance-authority";
 
-const CustomButton = () => {
-  return <div>CustomButton</div>;
-};
+// Fix 1: Correct the syntax and spelling of 'variants'.
+const classes = cva("border h-12 rounded-full px-6 font-medium", {
+  variants: {
+    variant: {
+      primary: "bg-lime-400 text-neutral-950 border-lime-400",
+      secondary: "border-white text-white bg-transparent",
+    },
+  },
+});
 
-export default CustomButton;
+export default function CustomButton(
+  props: {
+    variant: "primary" | "secondary";
+  } & HTMLAttributes<HTMLButtonElement>
+) {
+  const { variant, className, ...otherProps } = props;
+
+  return <button className={classes({ variant, className })} {...otherProps} />;
+}
