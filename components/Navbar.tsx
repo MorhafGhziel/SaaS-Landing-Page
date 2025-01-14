c9006b63399797b94f7194c01383c9444450e1b7
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-14 22:47:53 +0300

    centered the grid

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <section className="py-4">
       <div className="container">
-        <div className="grid grid-cols-2 border border-white/15 rounded-full">
+        <div className="grid grid-cols-2 border border-white/15 rounded-full p-2 px-4 items-center">
           <div>
             <Image src={logoImage} alt="Layers Logo" />
           </div>
