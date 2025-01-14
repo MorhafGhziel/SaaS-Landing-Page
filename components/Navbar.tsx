f42a5f5e23dca5539b70c11169a44e970267650c
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-14 23:43:17 +0300

    added pr at mid size to the grids

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <section className="py-4">
       <div className="container">
-        <div className="grid grid-cols-2 border border-white/15 rounded-full p-2 px-4 items-center">
+        <div className="grid grid-cols-2 border border-white/15 rounded-full px-4 p-2 md:pr-2 items-center">
           <div>
             <Image src={logoImage} alt="Layers Logo" className="h-9 w-auto" />
           </div>
