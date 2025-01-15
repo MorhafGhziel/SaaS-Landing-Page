950d3559572f554203f6b3e542fdcc7e7876d9b9
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-15 17:40:30 +0300

    added the hero paragraphs

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,15 @@ import React from "react";
 const Hero = () => {
   return (
     <section>
-      <div className="container"></div>
+      <div className="container">
+        <div>$7.5M seed round raised</div>
+        <h1>Impactful design, created effortlessly</h1>
+        <p>
+          Design tools shouldn't slow you down. Layers combines powerful
+          reatures with an intulive interace that keeps you in your creative
+          flow.
+        </p>
+      </div>
     </section>
   );
 };
