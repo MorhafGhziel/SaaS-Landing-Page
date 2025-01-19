783bf3308bee35602028f9063c4d4b245dbea841
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-19 20:15:21 +0300

    imported logoticker to th home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import LogoTicker from "@/components/LogoTicker";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <LogoTicker />
     </>
   );
 }
