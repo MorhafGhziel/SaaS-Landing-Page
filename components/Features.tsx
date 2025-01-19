91fb946de5520b06378675aace9540f81a7ef1a7
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-19 22:02:04 +0300

    wrraped the tag with a flex div to justify it center

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Tag from "./Tag";
 
 const Features = () => {
   return (
     <section>
-      <div className="container"></div>
+      <div className="container">
+        <div className="flex justify-center">
+          <Tag>Features</Tag>
+        </div>
+      </div>
     </section>
   );
 };
