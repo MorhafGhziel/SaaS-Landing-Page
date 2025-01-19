c57a03d97888a0961d409a418931a7b3e1f9c634
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-19 20:19:23 +0300

    added the title

--- components/LogoTicker.tsx
+++ components/LogoTicker.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const LogoTicker = () => {
   return (
     <section>
-      <div className="container"></div>
+      <div className="container">
+        <h3>Already chosen by these market leaders</h3>
+      </div>
     </section>
   );
 };
