4090be9c33d0fd7abd59e13b5d1dcf50826af84d
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-19 20:17:45 +0300

    created logos array to map over

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,26 @@
+import quantumLogo from "@/assets/images/quantum.svg";
+import acmeLogo from "@/assets/images/acme-corp.svg";
+import echoValleyLogo from "@/assets/images/echo-valley.svg";
+import pulseLogo from "@/assets/images/pulse.svg";
+import outsideLogo from "@/assets/images/outside.svg";
+import apexLogo from "@/assets/images/apex.svg";
+import celestialLogo from "@/assets/images/celestial.svg";
+import twiceLogo from "@/assets/images/twice.svg";
+
 export const navLinks = [
   { label: "Home", href: "#" },
   { label: "Features", href: "#features" },
   { label: "Integrations", href: "#integrations" },
   { label: "FAQs", href: "#faqs" },
+];
+
+export const logos = [
+  { name: "Quantum", image: quantumLogo },
+  { name: "Acme Corp", image: acmeLogo },
+  { name: "Echo Valley", image: echoValleyLogo },
+  { name: "Pulse", image: pulseLogo },
+  { name: "Outside", image: outsideLogo },
+  { name: "Apex", image: apexLogo },
+  { name: "Celestial", image: celestialLogo },
+  { name: "Twice", image: twiceLogo },
 ];
