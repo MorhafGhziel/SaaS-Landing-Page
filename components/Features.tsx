c5bbda4a7e968eb66de862317391f0457a431a24
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-20 01:35:39 +0300

    updated Features to use FeatureCard component

--- components/Features.tsx
+++ components/Features.tsx
@@ -15,29 +15,18 @@ const Features = () => {
           Where power meets <span className="text-lime-400">simplicity</span>
         </h2>
         <div>
-          <div>
-            <div></div>
-            <div>
-              <h3>Real-time Collaboration</h3>
-              <p>Work together seamlessly with contlict-tree team editing</p>
-            </div>
-          </div>
-          <div>
-            <div></div>
-            <div>
-              <h3>Interactive Prototyping</h3>
-              <p>
-                Engage your clients with prototypes that react to user actions
-              </p>
-            </div>
-          </div>
-          <div>
-            <div></div>
-            <div>
-              <h3>Keyboard Quick Actions</h3>
-              <p>Powerful commands to help you create desians more quickly</p>
-            </div>
-          </div>
+          <FeatureCard
+            title="Real-time Collaboration"
+            description="Work together seamlessly with contlict-tree team editing"
+          ></FeatureCard>
+          <FeatureCard
+            title="Interactive Prototyping"
+            description="Engage your clients with prototypes that react to user actions"
+          ></FeatureCard>
+          <FeatureCard
+            title="Keyboard Quick Actions"
+            description="Powerful commands to help you create desians more quickly"
+          ></FeatureCard>
         </div>
         <div>
           {features.map((feature) => (
