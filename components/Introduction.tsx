b84b4e11c7ea97d4673a51375bddd610de793ba4
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-19 21:43:53 +0300

    added some styles to the texts

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -9,7 +9,7 @@ const Introduction = () => {
         <div className="flex justify-center">
           <Tag>Introducing Layers</Tag>
         </div>
-        <div>
+        <div className="text-4xl text-center font-medium mt-10">
           <span>Your creative process deserves better.</span>
           <span> {text} </span>
           <span>That&apos;s why we built Layers.</span>
