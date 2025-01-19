7102035f1e672d8f0ac3929a5d2c69cf4bb29fcd
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-19 20:24:04 +0300

    done the map

--- components/LogoTicker.tsx
+++ components/LogoTicker.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { logos } from "@/constants";
+import Image from "next/image";
 
 const LogoTicker = () => {
   return (
     <section>
       <div className="container">
         <h3>Already chosen by these market leaders</h3>
+        <div>
+          {logos.map((logo) => (
+            <Image src={logo.image} key={logo.name} alt={logo.name} />
+          ))}
+        </div>
       </div>
     </section>
   );
