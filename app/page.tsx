2e3c6c820b27558cbce1fc1082e0abf25da07691
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-19 21:54:00 +0300

    imported the featurse to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import LogoTicker from "@/components/LogoTicker";
 import Introduction from "@/components/Introduction";
+import Features from "@/components/Features";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <LogoTicker />
       <Introduction />
+      <Features />
     </>
   );
 }
