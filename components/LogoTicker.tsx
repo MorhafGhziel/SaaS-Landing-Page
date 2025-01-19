861047adfbb7b71f4e3f588b733f73daa647f18c
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-19 20:26:21 +0300

    styled the title

--- components/LogoTicker.tsx
+++ components/LogoTicker.tsx
@@ -6,7 +6,9 @@ const LogoTicker = () => {
   return (
     <section className="py-24 overflow-x-clip">
       <div className="container">
-        <h3>Already chosen by these market leaders</h3>
+        <h3 className="text-center text-white/50 text-xl">
+          Already chosen by these market leaders
+        </h3>
         <div>
           {logos.map((logo) => (
             <Image src={logo.image} key={logo.name} alt={logo.name} />
