976b1c4968f34b2f7de7c3fc29cfc9d12bb8f1b9
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-22 21:12:36 +0300

    wrapped the title and the integrations to use grid

--- components/Integrations.tsx
+++ components/Integrations.tsx
@@ -7,21 +7,27 @@ const Integrations = () => {
   return (
     <section className="py-24 overflow-hidden">
       <div className="container">
-        <Tag>Integrations</Tag>
-        <h2 className="text-6xl font-medium mt-6">
-          Plays well with <span className="text-lime-400">others</span>
-        </h2>
-        <p className="text-white/50 mt-4 text-lg">
-          Layels seattless connects wil vour lavore loois, makine it easy to
-          plug into any workflow and collaborate across olatorms
-        </p>
-      </div>
-      <div className="h-[400px] mt-8 overflow-hidden grid md:grid-cols-2 gap-4 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]">
-        <IntegrationsColumn integrations={integrations} />
-        <IntegrationsColumn
-          integrations={integrations.slice().reverse()}
-          className="hidden md:flex"
-        />
+        <div>
+          <div>
+            <Tag>Integrations</Tag>
+            <h2 className="text-6xl font-medium mt-6">
+              Plays well with <span className="text-lime-400">others</span>
+            </h2>
+            <p className="text-white/50 mt-4 text-lg">
+              Layels seattless connects wil vour lavore loois, makine it easy to
+              plug into any workflow and collaborate across olatorms
+            </p>
+          </div>
+          <div>
+            <div className="h-[400px] mt-8 overflow-hidden grid md:grid-cols-2 gap-4 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]">
+              <IntegrationsColumn integrations={integrations} />
+              <IntegrationsColumn
+                integrations={integrations.slice().reverse()}
+                className="hidden md:flex"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
