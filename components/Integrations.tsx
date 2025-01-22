6b2b289e4cf370a31efed9a92ef27fd6a37954b8
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-22 21:02:47 +0300

    created integrationscolumn component

--- components/Integrations.tsx
+++ components/Integrations.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Tag from "./Tag";
 import { integrations } from "@/constants";
 import Image from "next/image";
+import IntegrationsColumn from "./IntegrationsColumn";
 
 const Integrations = () => {
   return (
@@ -19,31 +20,7 @@ const Integrations = () => {
         </p>
       </div>
       <div className="h-[400px] mt-8 overflow-hidden [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]">
-        <div className="flex flex-col gap-4 pb-4">
-          {integrations.map((integration) => (
-            <div
-              key={integration.name}
-              className="bg-neutral-900 border border-white/10 rounded-3xl p-6"
-            >
-              <div className="flex justify-center">
-                <Image
-                  src={integration.icon}
-                  alt={integration.name}
-                  width={96}
-                  height={96}
-                />
-              </div>
-              <h3 className="text-3xl text-center mt-6">
-                {" "}
-                {integration.name}{" "}
-              </h3>
-              <p className="text-center text-white/50 mt-2">
-                {" "}
-                {integration.description}{" "}
-              </p>
-            </div>
-          ))}
-        </div>
+        <IntegrationsColumn integrations={integrations} />
       </div>
     </section>
   );
