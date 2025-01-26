54137d3e7127fab827102224cc131a68170e7de5
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-26 22:18:23 +0300

    added the layers logo to the page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import logoImage from "@/public/logo.svg";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <section>
-      <div className="container"></div>
+      <div className="container">
+        <div>
+          <div>
+            <Image src={logoImage} alt="Layers Logo" />
+          </div>
+          <div></div>
+        </div>
+      </div>
     </section>
   );
 };
