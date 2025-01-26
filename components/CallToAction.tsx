1e676eecba51f45db7ff7cdd45c9f1040f946c22
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-26 22:09:05 +0300

    lopped over the start and the span

--- components/CallToAction.tsx
+++ components/CallToAction.tsx
@@ -3,10 +3,12 @@ import React from "react";
 const CallToAction = () => {
   return (
     <section>
-      <div>
-        <span>&#10038;</span>
-        <span>Try it for free</span>
-      </div>
+      {Array.from({ length: 10 }).map((_, i) => (
+        <div key={i}>
+          <span>&#10038;</span>
+          <span>Try it for free</span>
+        </div>
+      ))}
     </section>
   );
 };
