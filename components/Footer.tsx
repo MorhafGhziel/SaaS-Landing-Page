27afda0c609d9f615f3d29744ad8fa9dff5f49cf
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-26 22:22:23 +0300

    done the footer at small screen

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,16 +5,20 @@ import { footerLinks } from "@/constants";
 
 const Footer = () => {
   return (
-    <section>
+    <section className="py-16">
       <div className="container">
-        <div>
+        <div className="flex flex-col items-center gap-6">
           <div>
             <Image src={logoImage} alt="Layers Logo" />
           </div>
           <div>
-            {footerLinks.map((link) => (
-              <a href={link.href}>{link.label}</a>
-            ))}
+            <nav className="flex gap-6">
+              {footerLinks.map((link) => (
+                <a href={link.href} className="text-white/50 text-sm">
+                  {link.label}
+                </a>
+              ))}
+            </nav>
           </div>
         </div>
       </div>
