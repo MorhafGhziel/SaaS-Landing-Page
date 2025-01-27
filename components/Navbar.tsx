d518f1d4e662b12d5d5f1df16647bb5742ae9df8
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-27 16:08:53 +0300

    added bg and blur to the background

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <section className="py-4 lg:py-8 sticky top-0 z-50">
       <div className="container max-w-5xl">
-        <div className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full px-4 p-2 md:pr-2 items-center">
+        <div className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full px-4 p-2 md:pr-2 items-center bg-neutral-950/70 backdrop-blur">
           <div>
             <Image
               src={logoImage}
