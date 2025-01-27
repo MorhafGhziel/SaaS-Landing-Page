1e31897d2ad78ac6c20e5557d842169603e0667e
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-27 16:45:02 +0300

    added stroke width for better visibility

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -38,6 +38,7 @@ const Navbar = () => {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
+              strokeWidth="2"
               strokeLinecap="round"
               strokeLinejoin="round"
               className="feather feather-menu md:hidden"
