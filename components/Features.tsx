1181bd9a5936da91bb563132025eccfdbd25090c
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-30 00:56:00 +0300

    changed the duration values

--- components/Features.tsx
+++ components/Features.tsx
@@ -96,13 +96,13 @@ const Features = () => {
           >
             {" "}
             <div className="aspect-video flex items-center justify-center gap-4">
-              <Key className="w-28 outline outline-2 outline-offset-4 outline-transparent group-hover:outline-lime-400 transition-all duration-500">
+              <Key className="w-28 outline outline-2 outline-offset-4 outline-transparent group-hover:outline-lime-400 transition-all duration-300 ">
                 shift
               </Key>
-              <Key className="outline outline-2 outline-offset-4 outline-transparent group-hover:outline-lime-400 transition-all duration-500">
+              <Key className="outline outline-2 outline-offset-4 outline-transparent group-hover:outline-lime-400 transition-all duration-500 ">
                 alt
               </Key>
-              <Key className="outline outline-2 outline-offset-4 outline-transparent group-hover:outline-lime-400 transition-all duration-500">
+              <Key className="outline outline-2 outline-offset-4 outline-transparent group-hover:outline-lime-400 transition-all duration-700 ">
                 C
               </Key>
             </div>
