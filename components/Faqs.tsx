6067b1720cafd4e73e37814e50108d8aa57287c0
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-30 01:56:09 +0300

    added cursor pointer to the faqs

--- components/Faqs.tsx
+++ components/Faqs.tsx
@@ -18,7 +18,7 @@ const Faqs = () => {
           Questions? We&apos;ve got{" "}
           <span className="text-lime-400">answers</span>
         </h2>
-        <div className="mt-12 flex flex-col gap-6 max-w-xl mx-auto">
+        <div className="mt-12 flex flex-col gap-6 max-w-xl mx-auto cursor-pointer">
           {faqs.map((faq, faqIndex) => (
             <div
               key={faq.question}
