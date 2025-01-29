6533e3a9199b6b33421fd478ac66a6a53ff96750
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-29 23:29:48 +0300

    wrapped the introduction content with a div

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -6,15 +6,17 @@ const Introduction = () => {
   return (
     <section className="py-28 lg:py-40">
       <div className="container">
-        <div className="flex justify-center">
-          <Tag>Introducing Layers</Tag>
-        </div>
-        <div className="text-4xl md:text-6xl lg:text-7xl text-center font-medium mt-10">
-          <span>Your creative process deserves better.</span>
-          <span className="text-white/15"> {text} </span>
-          <span className="text-lime-400 block">
-            That&apos;s why we built Layers.
-          </span>
+        <div>
+          <div className="flex justify-center">
+            <Tag>Introducing Layers</Tag>
+          </div>
+          <div className="text-4xl md:text-6xl lg:text-7xl text-center font-medium mt-10">
+            <span>Your creative process deserves better.</span>
+            <span className="text-white/15"> {text} </span>
+            <span className="text-lime-400 block">
+              That&apos;s why we built Layers.
+            </span>
+          </div>
         </div>
       </div>
     </section>
