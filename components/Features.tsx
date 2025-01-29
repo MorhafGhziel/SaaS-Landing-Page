9c43c7c5b6933187a8516f95685675acdbb35531
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-30 00:45:55 +0300

    added duration

--- components/Features.tsx
+++ components/Features.tsx
@@ -72,7 +72,7 @@ const Features = () => {
             className="md:col-span-2 lg:col-span-1 group"
           >
             <div className="aspect-video flex items-center justify-center">
-              <p className="text-4xl font-extrabold text-white/20 group-hover:text-white/10 transition text-center">
+              <p className="text-4xl font-extrabold text-white/20 group-hover:text-white/10 transition duration-500 text-center">
                 We&apos;ve achieved{" "}
                 <span className="bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent relative">
                   <span>incredible</span>
@@ -82,7 +82,7 @@ const Features = () => {
                     loop
                     muted
                     playsInline
-                    className="absolute bottom-full left-1/2 -translate-x-1/2 rounded-2xl shadow-xl opacity-0 group-hover:opacity-100 transition pointer-events-none"
+                    className="absolute bottom-full left-1/2 -translate-x-1/2 rounded-2xl shadow-xl opacity-0 group-hover:opacity-100 transition duration-500 pointer-events-none"
                   />
                 </span>{" "}
                 growth this year
