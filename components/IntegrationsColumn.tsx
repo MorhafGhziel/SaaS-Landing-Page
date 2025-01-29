4e181ae4b400324d922f380d7c9c589d35ba7c83
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-01-30 01:16:01 +0300

    done the animation at the integrations column component

--- components/IntegrationsColumn.tsx
+++ components/IntegrationsColumn.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { Fragment } from "react";
 import { type IntegrationsType } from "@/types";
 import Image from "next/image";
 import { twMerge } from "tailwind-merge";
@@ -7,29 +9,50 @@ import { motion } from "framer-motion";
 const IntegrationsColumn = (props: {
   integrations: IntegrationsType;
   className?: string;
+  reverse?: boolean;
 }) => {
-  const { integrations, className } = props;
+  const { integrations, className, reverse } = props;
   return (
-    <motion.div className={twMerge("flex flex-col gap-4 pb-4", className)}>
-      {integrations.map((integration) => (
-        <div
-          key={integration.name}
-          className="bg-neutral-900 border border-white/10 rounded-3xl p-6"
-        >
-          <div className="flex justify-center">
-            <Image
-              src={integration.icon}
-              alt={integration.name}
-              width={96}
-              height={96}
-            />
-          </div>
-          <h3 className="text-3xl text-center mt-6"> {integration.name} </h3>
-          <p className="text-center text-white/50 mt-2">
-            {" "}
-            {integration.description}{" "}
-          </p>
-        </div>
+    <motion.div
+      initial={{
+        y: reverse ? "-50%" : 0,
+      }}
+      animate={{
+        y: reverse ? 0 : "-50%",
+      }}
+      transition={{
+        duration: 15,
+        repeat: Infinity,
+        ease: "linear",
+      }}
+      className={twMerge("flex flex-col gap-4 pb-4", className)}
+    >
+      {Array.from({ length: 2 }).map((_, i) => (
+        <Fragment key={i}>
+          {integrations.map((integration) => (
+            <div
+              key={integration.name}
+              className="bg-neutral-900 border border-white/10 rounded-3xl p-6"
+            >
+              <div className="flex justify-center">
+                <Image
+                  src={integration.icon}
+                  alt={integration.name}
+                  width={96}
+                  height={96}
+                />
+              </div>
+              <h3 className="text-3xl text-center mt-6">
+                {" "}
+                {integration.name}{" "}
+              </h3>
+              <p className="text-center text-white/50 mt-2">
+                {" "}
+                {integration.description}{" "}
+              </p>
+            </div>
+          ))}
+        </Fragment>
       ))}
     </motion.div>
   );
